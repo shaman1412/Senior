e61ef4facdb50a694facc8f5c184dec40ab06c66
rebellionfa <rebellionfa@gmail.com>
2017-03-15 16:31:18 +0700

    add chat room to party

--- app/src/main/java/com/Senior/Faff/Fragment/Party/Created_Fragment.java
+++ app/src/main/java/com/Senior/Faff/Fragment/Party/Created_Fragment.java
@@ -7,8 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.Senior.Faff.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +25,10 @@ import com.Senior.Faff.R;
 public class Created_Fragment extends Fragment {
 
     private static final String Tag = Created_Fragment.class.getSimpleName();
+
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
     private Button addRoom;
+    private EditText roomName;
 
     public Created_Fragment() {
         // Required empty public constructor
@@ -31,11 +40,17 @@ public class Created_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_created_, container, false);
+
+        roomName = (EditText) rootview.findViewById(R.id.AddRoomText);
+
         addRoom = (Button) rootview.findViewById(R.id.AddRoomButton);
         addRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(Tag, "Click add room!!!");
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put(roomName.getText().toString(),"");
+                root.updateChildren(map);
             }
         });
 
--- app/src/main/java/com/Senior/Faff/Fragment/Party/Room_fragment.java
+++ app/src/main/java/com/Senior/Faff/Fragment/Party/Room_fragment.java
@@ -1,12 +1,29 @@
 package com.Senior.Faff.Fragment.Party;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.Senior.Faff.R;
+import com.Senior.Faff.chat.Chat_Room;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,12 +33,16 @@ import com.Senior.Faff.R;
  */
 public class Room_fragment extends Fragment {
 
-
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private Context context;
+    private String name = "testuser";
 
     public Room_fragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +50,43 @@ public class Room_fragment extends Fragment {
         // Inflate the layout for this fragment
         View rootview =  inflater.inflate(R.layout.fragment_room_fragment, container, false);
 
+        listView = (ListView) rootview.findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,list_of_rooms);
+        listView.setAdapter(arrayAdapter);
+
+        root.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),Chat_Room.class);
+                intent.putExtra("room_name",((TextView)view).getText().toString() );
+                intent.putExtra("user_name",name);
+                startActivity(intent);
+            }
+        });
 
         return rootview;
     }
