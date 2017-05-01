0089bf0c7f6a2dd2319cffa548c0b3a0debc2d8f
rebellionfa <rebellionfa@gmail.com>
2017-05-01 16:21:45 +0700

    fix Search_Fragment

--- app/src/main/java/com/Senior/Faff/Fragment/SearchNearby/Search_name_recycle.java
+++ app/src/main/java/com/Senior/Faff/Fragment/SearchNearby/Search_name_recycle.java
@@ -3,6 +3,7 @@ package com.Senior.Faff.Fragment.SearchNearby;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,13 @@ import android.widget.TextView;
 
 import com.Senior.Faff.Fragment.Party.list_party_member;
 import com.Senior.Faff.R;
+import com.Senior.Faff.RestaurantProfile.Show_RestaurantProfile;
 import com.Senior.Faff.UserProfile.ShowUserprofile;
 import com.Senior.Faff.model.Restaurant;
 import com.Senior.Faff.model.UserProfile;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -34,15 +37,16 @@ public class Search_name_recycle extends RecyclerView.Adapter<Search_name_recycl
     private boolean host;
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-
-        public TextView list_name,open;
+        public LinearLayout item;
+        public TextView list_name,open,detail;
         public ImageView image;
         public ViewHolder(View v) {
             super(v);
+            item = (LinearLayout) v.findViewById(R.id.item);
             list_name = (TextView)v.findViewById(R.id.textView1);
             image =  (ImageView)v.findViewById(R.id.image);
             open = (TextView) v.findViewById(R.id.open_text);
-
+            detail = (TextView) v.findViewById(R.id.detail);
         }
     }
 
@@ -54,6 +58,7 @@ public class Search_name_recycle extends RecyclerView.Adapter<Search_name_recycl
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.restaurant_recycle_list, parent, false);
         ViewHolder vh = new ViewHolder(view);
+        mcontext = view.getContext();
         return vh;
     }
 
@@ -61,7 +66,31 @@ public class Search_name_recycle extends RecyclerView.Adapter<Search_name_recycl
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.list_name.setText(list.get(position).getRestaurantName());
         holder.open.setText(list.get(position).getPeriod());
+        holder.detail.setText(list.get(position).getDescription());
+        String url = list.get(position).getPicture();
+        String[] tmp = url.split(",");
 
+        if (tmp[0].contains(" "))
+        {
+            url = tmp[0].replaceAll(" ", "%20");
+            Log.i("TEST:", " url in search nearby : " +url);
+            Picasso.with(mcontext).load(url).resize(250, 250).into(holder.image);
+        }
+        else
+        {
+            Log.i("TEST:", " url in search nearby : " +tmp[0]);
+            Picasso.with(mcontext).load(tmp[0]).resize(250, 250).into(holder.image);
+        }
+
+        holder.item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(mcontext, Show_RestaurantProfile.class);
+                i.putExtra(Restaurant.Column.ResID, list.get(position).getresId());
+                i.putExtra(Restaurant.Column.UserID,list.get(position).getUserID());
+                mcontext.startActivity(i);
+            }
+        });
     }
 
     @Override
