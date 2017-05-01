722e9d91912c63cb6335b35caadafb545836e219
rebellionfa <rebellionfa@gmail.com>
2017-05-01 23:16:27 +0700

    fix Room fragment

---
+++ app/src/main/java/com/Senior/Faff/Fragment/Party/Room_Recycler.java
@@ -0,0 +1,180 @@
+package com.Senior.Faff.Fragment.Party;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.Senior.Faff.R;
+import com.Senior.Faff.UserProfile.ShowUserprofile;
+import com.Senior.Faff.model.Party;
+import com.Senior.Faff.model.UserProfile;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class Room_Recycler extends RecyclerView.Adapter<Room_Recycler.ViewHolder>{
+
+    Context mContext;
+    Context context;
+    ArrayList<Party> list;
+    private int count=0;
+    private int ccc=0;
+    public String userid;
+
+//    private String key;
+//    private String  setuserid;
+//    private String list_accept;
+//    private  Context mcontext;
+//    private boolean host;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ImageView image;
+        public LinearLayout mix;
+        public TextView appointment;
+        public TextView maxpeople;
+        public TextView RoomName;
+        public TextView currentpeople;
+        public TextView detail;
+        public ImageView imageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            imageView = (ImageView) v.findViewById(R.id.imageView1);
+            RoomName = (TextView) v.findViewById(R.id.textView1);
+            appointment = (TextView) v.findViewById(R.id.Location_text);
+            maxpeople = (TextView) v.findViewById(R.id.max);
+            currentpeople = (TextView) v.findViewById(R.id.people);
+            detail = (TextView) v.findViewById(R.id.detail);
+            mix = (LinearLayout) v.findViewById(R.id.mix);
+        }
+    }
+
+    public Room_Recycler(Context mcontext, ArrayList<Party> room_name, String userid) {
+        this.mContext = mcontext;
+        this.list = room_name;
+        this.userid = userid;
+
+        Log.i("TEST:", " In initial recyclerview");
+    }
+
+
+    @Override
+    public Room_Recycler.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_room__recycler, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        context = view.getContext();
+        return vh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+
+        Log.i("TEST:", " In recyclerview");
+
+        final String pic_url = list.get(position).getPicture();
+        if (pic_url != null) {
+            String[] tmp = pic_url.split("/");
+            StorageReference load = FirebaseStorage.getInstance().getReference().child(tmp[1]).child(tmp[2]);
+
+            load.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    // Got the download URL for 'users/me/profile.png'
+                    // Pass it to Picasso to download, show in ImageView and caching
+                    Log.i("TEST:", " recycler count : "+ccc);
+                    ccc++;
+                    try
+                    {
+                        Picasso.with(mContext).cancelRequest(holder.imageView);
+                        Picasso.with(mContext).load(uri.toString()).resize(250, 250).into(holder.imageView);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                    Log.i("TEST:", " recycler uri " + " : " + uri.toString() + " postition : "+position);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+
+                    Log.i("TEST: ", " recycler in List err " +  " : " + exception.toString());
+                    exception.printStackTrace();
+                }
+            });
+        }
+
+        if (list.get(position).getAccept() != null) {
+            String[] people = list.get(position).getAccept().split(",");
+            count = people.length;
+        }
+
+        holder.mix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, Show_party_profile.class);
+                intent.putExtra(Party.Column.RoomID, list.get(position).getRoomID());
+                intent.putExtra(UserProfile.Column.UserID, userid);
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.RoomName.setText(list.get(position).getName());
+        holder.appointment.setText(list.get(position).getAppointment());
+        holder.maxpeople.setText(Integer.toString(list.get(position).getPeople()));
+        holder.currentpeople.setText(Integer.toString(count));
+        holder.detail.setText("     " + list.get(position).getDescription());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public ArrayList<Party> getlist(){
+        return list;
+    }
+
+}
+
+
+
+//        extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_room__recycler);
+//    }
+//}
+
--- app/src/main/java/com/Senior/Faff/Fragment/Party/list_party_member.java
+++ app/src/main/java/com/Senior/Faff/Fragment/Party/list_party_member.java
@@ -21,131 +21,120 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class list_party_member extends RecyclerView.Adapter<list_party_member.ViewHolder>{
-    private ArrayList<UserProfile> list ;
+public class list_party_member extends RecyclerView.Adapter<list_party_member.ViewHolder> {
+    private ArrayList<UserProfile> list;
     private Context context;
     private String key;
-    private String  setuserid;
-    private  Context mcontext;
+    private String setuserid;
+    private Context mcontext;
     private boolean host;
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-                // each data item is just a string in this case
+        // each data item is just a string in this case
 
         public TextView list_name;
         public ImageView image;
         public Button kick;
         public LinearLayout mix;
+
         public ViewHolder(View v) {
             super(v);
-            list_name = (TextView)v.findViewById(R.id.name);
-            image =  (ImageView)v.findViewById(R.id.image);
+            list_name = (TextView) v.findViewById(R.id.name);
+            image = (ImageView) v.findViewById(R.id.image);
             kick = (Button) v.findViewById(R.id.kick);
-            mix = (LinearLayout)v.findViewById(R.id.mix);
+            mix = (LinearLayout) v.findViewById(R.id.mix);
         }
     }
 
-    public list_party_member(ArrayList<UserProfile> list, Context context, String key, boolean host){
-                this.list = list;
-                this.context = context;
-                this.key = key;
-                this.host = host;
+    public list_party_member(ArrayList<UserProfile> list, Context context, String key, boolean host) {
+        this.list = list;
+        this.context = context;
+        this.key = key;
+        this.host = host;
+    }
+
+    @Override
+    public list_party_member.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_list_party_accept, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        if (!host) {
+            View ac = view.findViewById(R.id.kick);
+            ac.setVisibility(View.INVISIBLE);
+        }
+        mcontext = view.getContext();
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+
+        String url = list.get(position).getPicture();
+        String tmp[] = url.split(",");
+        if (tmp[0].contains(" ")) {
+            url = tmp[0].replaceAll(" ", "%20");
+            Log.i("TEST:", " list party member url ");
+            try {
+                Picasso.with(mcontext).load(url).resize(150, 150).into(holder.image);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+        } else {
+            Log.i("TEST:", " list party member url ");
+            try {
+                Picasso.with(mcontext).load(tmp[0]).resize(150, 150).into(holder.image);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        holder.list_name.setText(list.get(position).getName());
+        holder.mix.setOnClickListener(new View.OnClickListener() {
             @Override
-            public list_party_member.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType ) {
-                View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.activity_list_party_accept, parent, false);
-                ViewHolder vh = new ViewHolder(view);
-                if(!host) {
-                    View ac = view.findViewById(R.id.kick);
-                    ac.setVisibility(View.INVISIBLE);
+            public void onClick(View v) {
+                Intent intent = new Intent(mcontext, ShowUserprofile.class);
+                intent.putExtra(UserProfile.Column.UserID, list.get(position).getUserid());
+                mcontext.startActivity(intent);
+            }
+        });
+        holder.kick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //holder.mix.setVisibility(View.GONE);
+                list.remove(position);
+                notifyDataSetChanged();
+                boolean first = true;
+                boolean change = false;
+                for (int i = 0; i < list.size(); i++) {
+                    if (first) {
+                        setuserid = list.get(i).getUserid();
+                        first = false;
+                        change = true;
+                    } else {
+                        setuserid = setuserid + ",";
+                        setuserid = setuserid + list.get(i).getUserid();
+                        change = true;
+                    }
                 }
-                mcontext = view.getContext();
-                return vh;
-            }
-
-            @Override
-            public void onBindViewHolder(final ViewHolder holder, final int position) {
-
-                String url = list.get(position).getPicture();
-                String tmp[] = url.split(",");
-                if(tmp[0].contains(" "))
-                {
-                    url = tmp[0].replaceAll(" ", "%20");
-                    Log.i("TEST:", " list party member url ");
-                    try
-                    {
-                        Picasso.with(mcontext).load(url).resize(150, 150).into(holder.image);
-                    }
-                    catch (Exception ex)
-                    {
-                        ex.printStackTrace();
-                    }
+                if (!change) {
+                    setuserid = null;
                 }
-                else
-                {
-                    Log.i("TEST:", " list party member url ");
-                    try
-                    {
-                        Picasso.with(mcontext).load(tmp[0]).resize(150, 150).into(holder.image);
-                    }
-                    catch (Exception ex)
-                    {
-                        ex.printStackTrace();
-                    }
-                }
-
-                holder.list_name.setText(list.get(position).getName());
-                holder.mix.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(mcontext, ShowUserprofile.class);
-                        intent.putExtra(UserProfile.Column.UserID,list.get(position).getUserid());
-                        mcontext.startActivity(intent);
-                    }
-                });
-                holder.kick.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //holder.mix.setVisibility(View.GONE);
-                        list.remove(position);
-                        notifyDataSetChanged();
-                        boolean first = true;
-                        boolean change = false;
-                        for(int i =0 ;i<list.size(); i++){
-                            if(first) {
-                                setuserid = list.get(i).getUserid();
-                                first =  false;
-                                change = true;
-                            }
-                            else {
-                                setuserid = setuserid + ",";
-                                setuserid = setuserid + list.get(i).getUserid();
-                                change = true;
-                            }
-                        }
-                        if (!change){
-                            setuserid = null;
-                        }
-                        sendRequest(setuserid);
-
-
-                    }
-                });
-
-
+                sendRequest(setuserid);
             }
+        });
+    }
 
-            @Override
-            public int getItemCount() {
-                return list.size();
-            }
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 
-            @Override
-            public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-                super.onAttachedToRecyclerView(recyclerView);
-            }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
 
-    public void sendRequest(String setuserid){
+    public void sendRequest(String setuserid) {
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("All_Room");
 
         mDatabase.child(key).child("accept").setValue(setuserid);
@@ -153,7 +142,7 @@ public class list_party_member extends RecyclerView.Adapter<list_party_member.Vi
     }
 
 
-    public ArrayList<UserProfile> getlist(){
-                return  list;
-            }
+    public ArrayList<UserProfile> getlist() {
+        return list;
+    }
 }
